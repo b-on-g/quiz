d76447e19dbe02b4e1a0f580af2d9377575e9174
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-24 07:54:36 +0000

    Deploying to gh-pages from @ b-on-g/quiz@d1e591742eb0a1fc296551fbfbf22b07ee233450 🚀

--- node.js
+++ node.js
@@ -12476,6 +12476,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
--- node.test.js
+++ node.test.js
@@ -12467,6 +12467,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
